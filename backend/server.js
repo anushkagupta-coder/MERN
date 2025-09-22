4a16770d8384d8f8b2c513fbeb38d310029e7cbd
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-22 18:24:15 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,25 @@
 import express from "express"
 
 const app= express();
+
+
 app.get("/api/notes",(req,res)=>{
-    res.send("you get 5 notes");
+    res.status(200).send("you get 500 notes");
+});
+
+app.post("/api/notes",(req,res)=> {
+    res.status(201).json({message:"note created succesfully"})
 })
+
+app.put("/api/notes:id",(req,res)=> {
+    res.status(200).json({message:"note updated succesfully"})
+})
+
+app.put("/api/notes:id",(req,res)=> {
+    res.status(200).json({message:"note deleted succesfully"})
+})
+
+
 
 app.listen(5001,()=> {
     console.log("server started on port:5001");
