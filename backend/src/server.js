5f74eec28cb6f17dbb2ec4034f717cb5d297d1c9
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-22 23:25:54 +0530

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,14 +8,15 @@ dotenv.config();
 console.log(process.env.MONGO_URI);
 
 const app= express();
+const PORT=process.env.PORT ||5001
 
 app.use("/api/notes",notesRoutes)
 
 connectDB();
 
 
-app.listen(5001,()=> {
-    console.log("server started on port:5001");
+app.listen(PORT,()=> {
+    console.log("server started on port:",PORT);
 });
 
 //mongodb+srv://anushka612004_db_user:Udi5VPrYtb0MCExn@cluster0.9elijjp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
