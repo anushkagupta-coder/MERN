b4669eb6766519244f9819e7b577cd8a024b5f77
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-22 21:52:27 +0530

    Update notesRoutes.js

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,6 +1,21 @@
 import express from "express"
 
-const router = express.Rpouter();
+const router = express.Router();
 
+router.get("/",(req,res) => {
+    res.status(200).send("you just fetched the notes");
+});
 
+router.post("/",(req,res)=> {
+    res.status(201).json({message:"note created succesfully"})
+});
+
+router.put("/:id",(req,res)=> {
+    res.status(200).json({message:"note updated succesfully"})
+});
+
+router.delete("/:id", (req,res)=> {
+    res.status(200).json({message:"note deleted succesfully"})
+});
+  
 export default router
