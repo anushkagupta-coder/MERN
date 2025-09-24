043d67efe0afb8f5dba0c8144f4510fc55129392
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-24 13:17:23 +0530

    Update notesController.js

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -2,7 +2,7 @@ import Note from "../models/Note.js";
 
 export async function getAllNotes(req,res) {
     try{
-        const  notes = await Note.find().sort({createAt:-1});//newest first
+        const  notes = await Note.find().sort({createdAt:-1});//newest first
         res.status(200).json(notes);
     }
     catch(error){
