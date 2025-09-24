02c5454a39e0897dfbd0c042779140a861f20084
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-24 11:14:13 +0530

    Update notesController.js

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -14,19 +14,28 @@ export async function getAllNotes(req,res) {
 export async function createNote(req,res) {
     try {
         const {title,content} =req.body;
-        const note=new Note({title,content});
+        const note=new Note({title : title,content: content});
 
         const savedNote=await note.save();
         res.status(201).json(savedNote);
 
-    } catch (error) {
+    } 
+    catch (error) {
         console.error("error in createNote controller",error);
         res.status(500).json({message:"internal server error"});
     }
 }
 
-export function updateNote(req,res) {
-    res.status(200).json({message:"note updated succesfully"})
+export async function updateNote(req,res) {
+    try{
+        const {title,content}= req.body
+        await Note.findByIdAndUpdate(req.params.id,{title,content})
+        res.status(200).json({message:"internal server error"});
+    }
+    catch (error) {
+        console.error("error in createNote controller",error);
+        res.status(500).json({message:"internal server error"});
+    }
 }
 
 export function deleteNote(req,res){
