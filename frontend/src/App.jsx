1d7dd882e2665106f4db76765f4c71c519e73037
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-27 00:01:05 +0530

    Update App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HomePage from ""
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </div>
+  )
+};
+
+export default App;
