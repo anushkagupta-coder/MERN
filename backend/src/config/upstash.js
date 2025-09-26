649b26f64d0c61b8d30f86a894e632f0312ab79f
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-26 23:33:14 +0530

    Update upstash.js

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -4,11 +4,11 @@ import { Redis } from "@upstash/redis";
 import dotenv from "dotenv" 
 dotenv.config()
 
-//create a ratelimiter that allows 10 requests per 20 seconds
+//create a ratelimiter that allows 100 requests per 60 seconds
 
 const ratelimit = new Ratelimit({
     redis: Redis .fromEnv(),
-    limiter:Ratelimit.slidingWindow(5,"10 s"),
+    limiter:Ratelimit.slidingWindow(100,"60 s"),
 });
 
 export default ratelimit;
