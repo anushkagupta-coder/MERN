a47bf668cffcd2084aca7d7c2ab7ef7ebbb15bc5
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-26 23:35:53 +0530

    Update rateLimiter.js

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -3,7 +3,7 @@ import ratelimt from "../config/upstash.js";
 const rateLimiter = async (req,resizeBy,next) => {
 
     try{
-        const {success}=await ratelimt.limit("my-limit-key");
+        const {success}=await ratelimt.limit("my-rate-limit");
 
         if(!success){
             return res.status(429).json({
