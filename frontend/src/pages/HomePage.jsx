f2ad87f8b6f8b072cfd1e5bc406f6fbd38f15d20
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-27 22:38:30 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,33 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Navbar from "../components/Navbar.jsx";
-
+import RateLimitedUI from "../components/RateLimitedUI.jsx";
+import axios from "axios";
 
 const HomePage=()=>{
-  return <div className = "min-h-screen">
+  const [isRateLimited,setIsRateLimited]=useState(false);
+  const [notes,setNotes]=useState([])
+  const [loading,setloading]=useState(true)
+
+  useEffect(() => {
+        const fetchNotes = async() =>{
+            try{
+                const res=await axios.post ("http://localhost:5173/api/notes")
+                
+                console.log(res);
+            } catch(error){
+                console.log("enter fetching notes");
+            }
+        }
+        fetchNotes();
+    
+    },[]);
+
+  return (
+    <div className = "min-h-screen">
         <Navbar/>
-        {isRateLimited &}
-    </div>;
+        {isRateLimited && <RateLimitedUI/>}
+    </div>
+  );
 };
 
 
