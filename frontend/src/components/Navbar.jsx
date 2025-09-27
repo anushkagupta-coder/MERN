4a66116c496376d0de83e82edc9ee812ad3e37ea
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-27 18:10:17 +0530

    Update Navbar.jsx

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+const Navbar=()=>{
+  
+  
+    return <header className ="bg-base-300 border-b border-base-content/10">
+        <div className="mx-auto max-w-6xl px-4">
+        
+        </>
+    </header>;
+};
+
+
+
+export default Navbar;
