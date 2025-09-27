46bf5d6dcce835381cd1b33e152bebef5d01dc21
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-27 23:00:34 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
 import {useEffect, useState} from "react";
 import Navbar from "../components/Navbar.jsx";
 import RateLimitedUI from "../components/RateLimitedUI.jsx";
-import axios from "axios"
+import axios from "lib/axios.js"
+import toast from "react-hot-toast";
 
 const HomePage=()=>{
   const [isRateLimited,setIsRateLimited]=useState(false);
@@ -11,11 +12,22 @@ const HomePage=()=>{
   useEffect(() => {
         const fetchNotes = async() =>{
             try{
-                const res=await axios.get ("http://localhost:5001/api/notes")
-                
+                const res=await axios.get ("http://localhost:5001/api/notes");
                 console.log(res.data);
+
+                setNotes(res.data)
+                setIsRateLimited(false)
             } catch(error){
-                console.log("enter fetching notes");
+                console.log("error fetching notes");
+                if(error.response.status===429){
+                    setIsRateLimited(true)
+                }
+                else{
+                    toast.error("failed to load notes")
+
+                }
+            }finally {
+                setLoading(false)
             }
         }
         fetchNotes();
